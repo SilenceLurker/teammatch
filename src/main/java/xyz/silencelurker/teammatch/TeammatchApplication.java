6f3f4a2660d4ba2069b5fe872b9654c6af6000da
Silence_Lurker <slvccans@gmail.com>
2022-09-28 20:30:39 +0800

    init

---
+++ src/main/java/xyz/silencelurker/teammatch/TeammatchApplication.java
@@ -0,0 +1,16 @@
+package xyz.silencelurker.teammatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Silence_Lurker slvccans@gmail.com
+ */
+@SpringBootApplication
+public class TeammatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeammatchApplication.class, args);
+	}
+
+}
